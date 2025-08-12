014fc921673d1af75395b384b55098d0c7e7ddb0
rey <njongremy30@gmail.com>
2025-08-12 11:59:42 +0100

    start of js frontend

---
+++ js/createroom.js
@@ -0,0 +1,7 @@
+const playerNameInput = document.querySelector('#playername');
+const submitNameButton = document.querySelector('.namebutton');
+let roomCode =  document.querySelector('.roomcode');
+let playerName = document.querySelector('.playername');
+const returnButton = document .querySelector('.return');
+const startButton = document.querySelector('.startgame');
+
